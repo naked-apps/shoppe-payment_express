55ee4b6d08516fd37382e359b11ba4c10d0fdc15
Brendan Kilfoil <brendan@nakedapps.co.nz>
2015-11-29 14:30:45 +1300

    Rename route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   scope module: :shoppe do
     namespace :paymentexpress do
       get 'pay/:order_token', to: 'payments#pay', as: 'payment'
-      post 'pay/callback', to: 'payments#process_callback', as: 'callback'
+      post 'callback', to: 'payments#process_callback', as: 'callback'
     end
   end
 end
